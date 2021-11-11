1b5030e2fd0e5863d9e3a459052f6f3ff2e59119
Luis Wirth <lwirth2000@gmail.com>
2021-11-11 18:04:22 +0100

    render pdf with poppler

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 use cairo::{ImageSurface, Format, Context};
+use poppler::PopplerDocument;
 use std::fs::File;
 
 fn main() {
     let surface = ImageSurface::create(Format::ARgb32, 600, 200).unwrap();
-    let context = Context::new(&surface).unwrap();
+    let context = Context::new(&surface);
 
-    context.set_source_rgb(1.0, 1.0, 1.0);
-    context.paint().unwrap();
+    let poppler_document = PopplerDocument::new_from_file("/home/luis/tmp/poppler.pdf", "").unwrap();
+    let poppler_page = poppler_document.get_page(0).unwrap();
 
-    context.set_source_rgb(0.0, 0.0, 0.0);
-    for y in 0..100 {
-        let yf = y as f64 / 100.0 * 600.0;
-        for x in 0..100 {
-            let xf = x as f64 / 100.0 * 200.0;
-            context.line_to(xf, yf);
-        }
-    }
-    context.stroke().unwrap();
+    poppler_page.render(&context);
 
     let mut file = File::create("/home/luis/tmp/cairo.png").unwrap();
     surface.write_to_png(&mut file).unwrap();
+
 }
